44096516adb3065454cb7c0ef1ae71dd91fe6429
BLMX6 <119624247+BLMX6@users.noreply.github.com>
2022-12-05 12:00:11 -0600

    Update LoginScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -43,7 +43,7 @@ export default class LoginScreen extends Component {
             .auth()
             .signInWithEmailAndPassword(email, password)
             .then(() => {
-                this.props.navigation.replace("Dashboard");
+                this.props.navigation.replace("Panel");
             })
             .catch(error => {
                 Alert.alert(error.message);
@@ -65,14 +65,14 @@ export default class LoginScreen extends Component {
                 <TextInput
                     style={styles.textinput}
                     onChangeText={text => this.setState({ email: text })}
-                    placeholder={"Enter Email"}
+                    placeholder={"Ingresar correo electrónico"}
                     placeholderTextColor={"#FFFFFF"}
                     autoFocus
                 />
                 <TextInput
                     style={[styles.textinput, { marginTop: 20 }]}
                     onChangeText={text => this.setState({ password: text })}
-                    placeholder={"Enter Password"}
+                    placeholder={"Ingresar contraseña"}
                     placeholderTextColor={"#FFFFFF"}
                     secureTextEntry
                 />
@@ -80,12 +80,12 @@ export default class LoginScreen extends Component {
                     style={[styles.button, { marginTop: 20 }]}
                     onPress={() => this.signIn(email, password)}
                 >
-                    <Text style={styles.buttonText}>Login</Text>
+                    <Text style={styles.buttonText}>Inicio de sesión</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
-                    onPress={() => this.props.navigation.navigate("RegisterScreen")}
+                    onPress={() => this.props.navigation.navigate("PantallaRegistro")}
                 >
-                    <Text style={styles.buttonTextNewUser}>New User ?</Text>
+                    <Text style={styles.buttonTextNewUser}>¿Usuario nuevo?</Text>
                 </TouchableOpacity>
             </View>
         );
